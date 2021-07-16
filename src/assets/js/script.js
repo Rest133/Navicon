6129272dd9f980f7ae62a83376ee3bfc900e91b5
Rest133 <31849309+Rest133@users.noreply.github.com>
2021-07-16 23:31:38 +0300

    End of layout main page. Need some fix

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     addPortfolioSliderHandler()
 
     addPartnersPortfolio()
+
+    changeHeaderBg()
 })
 
 
@@ -11,11 +13,11 @@ function addPortfolioSliderHandler() {
 
         allSliders.forEach(slider => {
             $(slider).slick({
-                arrows:true,
-                slidesToShow:5,
-                infinite:false,
-                variableWidth:true,
-                draggable:false
+                arrows: true,
+                slidesToShow: 5,
+                infinite: false,
+                variableWidth: true,
+                draggable: false
             })
         })
     }
@@ -25,11 +27,36 @@ function addPartnersPortfolio() {
     if (document.querySelector('.partners__slider') !== null) {
         let slider = document.querySelector('.partners__slider')
         $(slider).slick({
-            arrows:true,
-            slidesToShow:3,
-            infinite:false,
-            adaptiveHeight:true,
-            draggable:false
+            arrows: true,
+            slidesToShow: 3,
+            infinite: false,
+            adaptiveHeight: true,
+            draggable: false
+        })
+    }
+}
+
+function changeHeaderBg() {
+    let allBlock = document.querySelectorAll('.block'),
+        header = document.querySelector('header')
+    if (window.matchMedia('(min-width: 768px)').matches) {
+
+        window.addEventListener('scroll', () => {
+            allBlock.forEach(block => {
+                if (block.getBoundingClientRect().bottom >= 120 && header.getBoundingClientRect().bottom >= block.getBoundingClientRect().y) {
+                    if (block.classList.contains('block_dark')) {
+                        header.classList.add('fixed')
+                        header.classList.remove('fixed-white')
+                    } else {
+                        header.classList.add('fixed-white')
+                    }
+                }
+            })
+
+            if (header.getBoundingClientRect().top + pageYOffset === 0) {
+                header.classList.remove('fixed')
+                header.classList.remove('fixed-white')
+            }
         })
     }
 }
